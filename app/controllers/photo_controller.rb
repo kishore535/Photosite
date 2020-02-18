70abab0d9534f06e3317307d4e6fd9fbbab93a0a
kishore535 <kishorereddy535@gmail.com>
2020-02-17 22:25:58 -0800

    Photosite project -commit -1

---
+++ app/controllers/photo_controller.rb
@@ -0,0 +1,8 @@
+class PhotoController < ApplicationController
+  def index
+    @users = User.joins(:photos)
+    @users = User.find(params[:id])
+    @photo = Photo.joins(:comments)
+    @photo = @users.photos
+  end
+end
---
+++ db/migrate/20200215024849_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :photos do |t|
+      t.column :user_id, :string
+      t.column :date_time, :string
+      t.column :file_name, :string
+      t.timestamps null: false
+    end
+  end
+end
